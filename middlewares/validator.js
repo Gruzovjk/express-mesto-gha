5463f7524d3ac06862e33dcc7a2ae23e8325216a
Land Lover <filimonovebay@gmail.com>
2023-05-05 00:04:18 +0300

    fix: body/params celebrate

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -41,13 +41,13 @@ const validateCardCreate = celebrate({
 });
 
 const validateUserId = celebrate({
-  body: Joi.object().keys({
+  params: Joi.object().keys({
     userId: Joi.string().hex().length(24).required(),
   }),
 });
 
 const validateCardId = celebrate({
-  body: Joi.object().keys({
+  params: Joi.object().keys({
     cardId: Joi.string().hex().length(24).required(),
   }),
 });
