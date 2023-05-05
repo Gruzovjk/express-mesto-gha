40869d492bffc9450e36ab04023c2172077b455c
Land Lover <filimonovebay@gmail.com>
2023-05-05 16:29:20 +0300

    test: валидация ссылки на картинку

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,7 +19,7 @@ module.exports.createCard = (req, res, next) => {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(200).send({ card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
         const error = new BadRequestError(
           `Переданы некорректные данные - ${err.name - err.message}`,
         );
